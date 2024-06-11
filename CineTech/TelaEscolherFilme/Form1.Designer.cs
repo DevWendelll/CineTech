8363f268ff39605b1c36448d81bdd021712add9f
wendell <wendellnascarneiro16@gmail.com>
2024-06-11 20:38:48 -0300

    rsrsr

---
+++ CineTech/TelaEscolherFilme/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace TelaEscolherFilme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbFilmes = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.imgCapaFilme = new System.Windows.Forms.PictureBox();
+            this.pnlNomeData = new System.Windows.Forms.Panel();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCapaFilme)).BeginInit();
+            this.pnlNomeData.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(50, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ICineTech";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(462, 243);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bem vindo, ao \r\nnosso app de \r\nfilmes\r\n";
+            // 
+            // cmbFilmes
+            // 
+            this.cmbFilmes.FormattingEnabled = true;
+            this.cmbFilmes.Location = new System.Drawing.Point(61, 377);
+            this.cmbFilmes.Name = "cmbFilmes";
+            this.cmbFilmes.Size = new System.Drawing.Size(213, 20);
+            this.cmbFilmes.TabIndex = 2;
+            this.cmbFilmes.SelectedIndexChanged += new System.EventHandler(this.cmbFilmes_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(1240, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnConfirmar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirmar.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.ForeColor = System.Drawing.Color.White;
+            this.btnConfirmar.Location = new System.Drawing.Point(61, 415);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(93, 31);
+            this.btnConfirmar.TabIndex = 4;
+            this.btnConfirmar.Text = "Comfirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // imgCapaFilme
+            // 
+            this.imgCapaFilme.Location = new System.Drawing.Point(629, 58);
+            this.imgCapaFilme.Name = "imgCapaFilme";
+            this.imgCapaFilme.Size = new System.Drawing.Size(597, 548);
+            this.imgCapaFilme.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgCapaFilme.TabIndex = 5;
+            this.imgCapaFilme.TabStop = false;
+            // 
+            // pnlNomeData
+            // 
+            this.pnlNomeData.Controls.Add(this.button1);
+            this.pnlNomeData.Controls.Add(this.label5);
+            this.pnlNomeData.Controls.Add(this.label4);
+            this.pnlNomeData.Controls.Add(this.textBox1);
+            this.pnlNomeData.Controls.Add(this.maskedTextBox1);
+            this.pnlNomeData.Location = new System.Drawing.Point(50, 463);
+            this.pnlNomeData.Name = "pnlNomeData";
+            this.pnlNomeData.Size = new System.Drawing.Size(324, 228);
+            this.pnlNomeData.TabIndex = 6;
+            this.pnlNomeData.Visible = false;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(11, 88);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(166, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Seu nome";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Data de Nascimento";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.BlueViolet;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(11, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Assisitir agora!";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 738);
+            this.Controls.Add(this.pnlNomeData);
+            this.Controls.Add(this.imgCapaFilme);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbFilmes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("TechnicBold", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgCapaFilme)).EndInit();
+            this.pnlNomeData.ResumeLayout(false);
+            this.pnlNomeData.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbFilmes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.PictureBox imgCapaFilme;
+        private System.Windows.Forms.Panel pnlNomeData;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CineTech/TelaEscolherFilme/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelaEscolherFilme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarFilmes();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+
+            if (btnConfirmar.Enabled)
+            {
+                pnlNomeData.Visible = true;
+            }
+        }
+        private void CarregarFilmes()
+        {
+            // Adicione os filmes à ComboBox
+            cmbFilmes.Items.Add("Velozes e Furiosos 10");
+            cmbFilmes.Items.Add("Sonic 2");
+            cmbFilmes.Items.Add("Filme 3");
+
+            // Configure o evento SelectedIndexChanged
+            cmbFilmes.SelectedIndexChanged += cmbFilmes_SelectedIndexChanged;
+        }
+    
+
+        private void cmbFilmes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Obtenha o filme selecionado
+            string filmeSelecionado = cmbFilmes.SelectedItem.ToString();
+
+            // Mude a imagem da PictureBox de acordo com o filme selecionado
+            switch (filmeSelecionado)
+            {
+                case "Velozes e Furiosos 10":
+                    imgCapaFilme.Image = Image.FromFile("velozesEfuriosos.jpg");
+                    break;
+                case "Sonic 2":
+                    imgCapaFilme.Image = Image.FromFile("imagens.sonic.jpg");
+                    break;
+                case "Filme 3":
+                    imgCapaFilme.Image = Image.FromFile("caminho_para_imagem_do_filme3.jpg");
+                    break;
+                default:
+                    imgCapaFilme.Image = null;
+                    break;
+            }
+        }
+    }
+}
